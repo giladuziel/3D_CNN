34fddb079e9e4e59a8629c77923b8cf3cc420454
Gilad <gilad@gmail.com>
2018-03-21 13:36:51 +0200

    add settings

--- classifier_3d.py
+++ classifier_3d.py
@@ -5,71 +5,9 @@ import os
 import sys
 import pickle
 from collections import Counter
+from settings import *
 
 
-AUGMENTED_VOXELS = 0
-
-TIRE_1_CONV_OUTPUT = 20  #need to be 20
-
-TIRE_2_CONV_OUTPUT = 30  #need to be 30
-
-TIRE_3_CONV_OUTPUT = TIRE_2_CONV_OUTPUT
-
-EPOCHS = 30  #need to be 30
-
-WINDOWS_SIZE = 2
-
-SAVING_INTERVAL = 100  #need to be 100
-
-MEAN = 0.0
-
-STDDEV = 0.05
-
-FILTER_WIDTH = 3
-
-FILTER_HEIGHT = 3
-
-FILTER_DEPTH = 3
-
-SMALL_FILTER_WIDTH = 1
-
-SMALL_FILTER_HEIGHT = 1
-
-SMALL_FILTER_DEPTH = 1
-
-BIG_FILTER_WIDTH = 5
-
-BIG_FILTER_HEIGHT = 5
-
-BIG_FILTER_DEPTH = 5
-
-CHANNELS = 1
-
-CAD_WIDTH = 30
-
-CAD_HEIGHT = 30
-
-CAD_DEPTH = 30
-
-OUTPUT_SIZE = 64 # need to be 64
-
-BATCH_SIZE = 12
-
-LEARNING_RATE = BATCH_SIZE * 0.0001
-
-TARGET_ERROR_RATE = 0.001
-
-LIMIT = 5000 # need to be 5000
-
-FC_NEURONS = 2048 # need to be 2048
-
-COST_FUNCTION = "cross"  #cross/sqr
-
-NUMBER_OF_TARGETS = 10
-
-TRAIN_DAT_SET = "train_cad_10.tar.gz"
-
-TEST_DATA_SET = "test_cad_10.tar.gz"
 
 try:
     from local_settings import *
